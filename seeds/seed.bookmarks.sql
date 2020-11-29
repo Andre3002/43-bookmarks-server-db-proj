4e85c975d7b053bfcd1d2a32909662d13e848f28
Andre <afowlk1@gmail.com>
2020-11-29 18:15:55 -0500

    Complete up to get/ articles

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,32 @@
+
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES (
+        'First bookmark!',
+        'www.test.com',
+        'Lorem ipsum dolor sit amet',
+        5
+    ),
+    (
+        'Second bookmark!',
+        'www.test2.com',
+        'Cum, exercitationem cupiditate',
+        1
+    ),
+    (
+        'Third bookmark!',
+        'www.test3.com',
+        'Possimus, voluptate ipsa dignissimos',
+        2
+    ),
+    (
+        'Fourth bookmark',
+        'www.test4.com',
+        'Aspernatur, quis nihil',
+        3
+    ),
+    (
+        'Fifth bookmark',
+        'www.test5.com',
+        'Amet soluta fugiat itaque recusandae',
+        4
+    );
